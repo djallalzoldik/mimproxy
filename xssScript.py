a0d0fb9534730122011e2b0db48b8db884dfa509
Djallal Fekirine <45321671+djallalzoldik@users.noreply.github.com>
2023-12-28 17:38:06 +0100

    Update xssScript.py

--- xssScript.py
+++ xssScript.py
@@ -10,12 +10,18 @@ flow_dir = "saved_flows"
 os.makedirs(flow_dir, exist_ok=True)
 
 # Define the token and parameter to mark altered requests
-altered_token = "_altered123"
-altered_header = "x-altered-flow"  # Lowercase to comply with HTTP/2
+alteration_indicator = "ex'\"><h1>akira</h1>"
+altered_header = "x-altered"
 
 def response(flow: http.HTTPFlow) -> None:
+    # Check if the response is HTML
+    content_type = flow.response.headers.get("Content-Type", "")
+    if "text/html" not in content_type:
+        return  # Skip non-HTML responses
+
     # Check if the request has already been altered
     if altered_header in flow.request.headers:
+        check_altered_reflection(flow)
         return
 
     # Handle GET and POST requests
@@ -41,25 +47,27 @@ def process_parameters(flow, params):
     for param, value in params.items():
         if isinstance(value, list):  # Handle lists for form data
             value = value[0]
-        # Ensure that the value is a string before checking for reflections
         if isinstance(value, str) and value in flow.response.text:
-            altered_flow = alter_request(flow, param, value)
-            if altered_flow:
-                ctx.master.commands.call("replay.client", [altered_flow])
-                save_flow(altered_flow)  # Save the altered flow
+            alter_and_replay(flow, param, value)
 
-def alter_request(original_flow, param, original_value):
+def alter_and_replay(original_flow, param, original_value):
+    altered_value = original_value + alteration_indicator
+    altered_flow = alter_request(original_flow, param, altered_value)
+    if altered_flow:
+        ctx.master.commands.call("replay.client", [altered_flow])
+
+def alter_request(original_flow, param, altered_value):
     new_request = copy.deepcopy(original_flow.request)
-    altered_value = original_value + altered_token
 
     if original_flow.request.method == "GET":
         new_request.query[param] = altered_value
     else:
-        if "application/x-www-form-urlencoded" in new_request.headers.get("Content-Type", ""):
+        content_type = new_request.headers.get("Content-Type", "")
+        if "application/x-www-form-urlencoded" in content_type:
             params = urllib.parse.parse_qs(new_request.get_text())
             params[param] = [altered_value]
             new_request.text = urllib.parse.urlencode(params, doseq=True)
-        elif "application/json" in new_request.headers.get("Content-Type", ""):
+        elif "application/json" in content_type:
             try:
                 body = json.loads(new_request.get_text())
                 if param in body and isinstance(body[param], str):
@@ -69,14 +77,18 @@ def alter_request(original_flow, param, original_value):
                 pass
 
     # Mark the request as altered
-    new_request.headers[altered_header] = altered_token
+    new_request.headers[altered_header] = "true"
 
     altered_flow = http.HTTPFlow(original_flow.client_conn, original_flow.server_conn)
     altered_flow.request = new_request
     return altered_flow
 
+def check_altered_reflection(flow):
+    if alteration_indicator in flow.response.text:
+        ctx.log.info(f"Altered parameter reflected in the response: {flow.request.url}")
+        save_flow(flow)  # Save the altered flow
+
 def save_flow(flow):
-    # Generate a unique identifier for each flow based on its details
     identifier = f"{flow.request.method}_{flow.request.host}_{flow.request.path}".replace("/", "_")
     filename = os.path.join(flow_dir, f"{identifier}.mitm")
     try:
